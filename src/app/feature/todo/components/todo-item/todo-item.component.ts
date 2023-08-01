c78e5fac00cf9d5e9ccf6a4fc2e0038eb81816e9
Airam <airam.dev@gmail.com>
2023-08-01 17:48:44 +0100

    Allows to edit todo

--- src/app/feature/todo/components/todo-item/todo-item.component.ts
+++ src/app/feature/todo/components/todo-item/todo-item.component.ts
@@ -1,19 +1,47 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ITodo } from '../../models/todo.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss']
 })
-export class TodoItemComponent {
-  @Input({required: true}) todo!: ITodo;
+export class TodoItemComponent implements OnInit {
+  @Input({ required: true }) todo!: ITodo;
   @Output() toggleCompleted = new EventEmitter<number>();
+  @ViewChild('inputText') inputText!: ElementRef;
+
+  protected textControl!: FormControl<string>;
+  protected isEditingControl!: FormControl<boolean>;
+
+  ngOnInit(): void {
+    this.textControl = new FormControl<string>(this.todo!.text ?? '', {
+      nonNullable: true,
+      validators: Validators.required
+    });
+    this.isEditingControl = new FormControl<boolean>(false, { nonNullable: true });
+  }
+
+  protected get isEditing(): boolean {
+    return this.isEditingControl.value;
+  }
 
   protected onToggleCompleted(id: number): void {
     this.toggleCompleted.emit(id);
+  }
+
+  protected onSetEditing(): void {
+    this.isEditingControl.setValue(true);
+    setTimeout(() => {
+      this.inputText.nativeElement.select();
+    })
+  }
+
+  protected onCloseEditing(): void {
+    this.isEditingControl.setValue(false);
   }
 }
